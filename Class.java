b8c0260d85ab415fdc6141dc360fe6b893e60036
PaVline <pavline1708@gmail.com>
2018-09-27 22:05:16 +0400

    Stef one love

---
+++ Class.java
@@ -0,0 +1,7 @@
+package RPIS61.Bezukhova.wdad;
+public class Class {
+
+    public static void main(String[] args){
+        System.out.println("I'm Bezukhova Polina, and I'm razgil'dyaika!");
+    }
+}
